dd01ad394e17decb63dcede403e221444e4b64f6
Amanda Abreu <abreuamandasec@gail.com>
2024-08-20 21:43:55 -0300

    desafio complementar, aula 02

---
+++ script.js
@@ -0,0 +1,6 @@
+const valorString = prompt("Informe seu nome:");
+const valorNumerico = parseInt(prompt("Informe sua idade:"));
+
+const anoNascimento = 2024 - valorNumerico;
+
+console.log('Olá, ',valorString,'! Você nasceu em',anoNascimento,'.')
